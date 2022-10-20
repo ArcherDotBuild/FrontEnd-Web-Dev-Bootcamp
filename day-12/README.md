21a5680cb39df454e3d8ea7f00517a337aa95d45
ElfGod <albertogx@icloud.com>
2022-10-19 23:32:32 -0500

    day 12 start

---
+++ day-12/README.md
@@ -0,0 +1,24 @@
+![image info](./welcome-day-12.png)
+
+# Welcome to Day 12
+
+## **Promises, Async/Await**
+
+## 1. Files Order
+
+- first: promises
+
+## Resources:
+
+- Enumerability: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Enumerability_and_ownership_of_properties
+- Object.defineProperty: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+- Intro to promises: https://web.dev/promises/
+- Promise methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise#constructor
+- Resolve Promise: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
+- Reject Promise: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/reject
+- JSON: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+- Fetch api: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+- Try Catch: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+- Set: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+- Maps: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+- Array methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#instance_methods
---
+++ day-12/promises/promises.js
@@ -0,0 +1,57 @@
+// Example 1
+// let promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10_000) // kept my promise
+//     reject(new Error("I lied, you're not getting your money"))
+//   }, 2000) // represent two days
+// })
+
+// promise
+//   .then((money) => {
+//     if (money === 10_000) {
+//       console.log('Yay!, i got my money back')
+//     }
+//   })
+//   .catch((error) => {
+//     console.error('😢', error.message)
+//   })
+
+console.log('\n')
+// Example 2
+let moneyLended = 10_000
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10_000) // kept my promise
+    reject(new Error("I lied, you're not getting your money"))
+  }, 2000) // represent two days
+})
+
+// Error aqui minuto 25
+promise
+  .then((money) => {
+    if (money === moneyLended) {
+      console.log('Yay!, i got my money back')
+    }
+    return money
+  })
+  .then(moneyReceived)
+  .catch((error) => {
+    console.error('😢', error.message)
+  })
+
+// Fullfilled state - successful outcome
+// Rejected state - error
+// Pending state - neither fullfilled nor rejected
+// settled state - it represents - fullfilled state / rejected state
+
+function moneyReceived(amountReceived) {
+  return new Promise((resolve, reject) => {
+    if (amountReceived > moneyLended) {
+      resolve(amountReceived - moneyLended)
+    } else if (amountReceived < moneyLended) {
+      reject(new Error('Oh wait, i have received the entire amount'))
+    } else {
+      resolve(0)
+    }
+  })
+}
