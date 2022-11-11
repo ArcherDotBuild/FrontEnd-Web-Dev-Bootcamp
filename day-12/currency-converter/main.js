d7c22a033320f9ed1f4a76cf70b70317cc1f97e0
ElfGod <albertogx@icloud.com>
2022-11-10 23:53:06 -0500

    day12 par7 async await

--- day-12/currency-converter/main.js
+++ day-12/currency-converter/main.js
@@ -15,42 +15,44 @@ document.addEventListener('DOMContentLoaded', () => {
         method: 'GET',
         headers,
       }
+      try {
+        let response = await fetch(
+          `https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`,
+          requestOptions
+        )
+        const data = await response.json()
+        // console.log(data)
+        let {
+          info,
+          date,
+          query: { to: convertedTo },
+          result,
+        } = data
 
-      let response = await fetch(
-        `https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`,
-        requestOptions
-      )
-      const data = await response
-        .json()
-        .then((response) => response.json())
-        // .then((data) => console.log(data))
-        .then((data) => {
-          // console.log(data)
-          // {
-          //     "success": true,
-          //     "query": {
-          //         "from": "EUR",
-          //         "to": "INR",
-          //         "amount": 100
-          //     },
-          //     "info": {
-          //         "timestamp": 1667973004,
-          //         "rate": 82.100901
-          //     },
-          //     "date": "2022-11-09",
-          //     "result": 8210.0901
-          // }
-          let {
-            info,
-            date,
-            query: { to },
-            result,
-          } = data
-          document.querySelector(
-            '.result'
-          ).textContent = `As per the exchage rate:${info.rate} for ${date} => converted value in ${to} is ${result}`
-        })
-        .catch((error) => console.log(error))
-      console.log('end of function')
+        document.querySelector(
+          '.result'
+        ).textContent = `As per the exchage rate:${info.rate} for ${date} => converted value in ${convertedTo} is ${result}`
+      } catch (error) {
+        console.log(error)
+      } finally {
+        console.log('finally block')
+      }
     })
+
+  console.log('end of function')
 })
+
+// {
+//     "success": true,
+//     "query": {
+//         "from": "EUR",
+//         "to": "INR",
+//         "amount": 100
+//     },
+//     "info": {
+//         "timestamp": 1667973004,
+//         "rate": 82.100901
+//     },
+//     "date": "2022-11-09",
+//     "result": 8210.0901
+// }
