e1f98658f60fceed537c113ab162ebe91dd9c98d
ElfGod <albertogx@icloud.com>
2024-10-24 18:30:05 -0500

    Day 12 part2 promises examples done

--- day-12/01-promises/promises.js
+++ day-12/01-promises/promises.js
@@ -32,6 +32,8 @@ promiseOne
     console.error('😢', error.message)
   })
 
+console.log('----- END -----')
+
 console.log('\n')
 console.log('####################')
 // TODO: example 2
@@ -60,6 +62,8 @@ promiseTwo
   .catch((error) => {
     console.error('😢', error.message)
   })
+
+console.log('----- END -----')
 
 console.log('\n')
 console.log('####################')
@@ -105,123 +109,133 @@ function moneyReceived(amountReceived) {
   })
 }
 
-console.log('\n')
-// Example 4
-// let moneyLended = 10_000
-// let promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     // resolve(10_000) // kept my promise
-//     reject(new Error("I lied, you're not getting your money"))
-//   }, 2000) // represent two days
-// })
-
-// promise.then(
-//   (money) => {
-//     console.log('Yay!, i got my money back')
-//     return money
-//   },
-//   (error) => {
-//     console.error('😢', error.message)
-//   }
-// )
-
-// function moneyReceived(amountReceived) {
-//   return new Promise((resolve, reject) => {
-//     if (amountReceived > moneyLended) {
-//       resolve(amountReceived - moneyLended)
-//     } else if (amountReceived < moneyLended) {
-//       reject(new Error('Oh wait, i have not received the entire amount'))
-//     } else {
-//       resolve(0)
-//     }
-//   })
-// }
+console.log('----- END -----')
 
 console.log('\n')
-// Example 5
-// let moneyLended = 10_000
-// let promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve(10_000) // kept my promise
-//     // reject(new Error("I lied, you're not getting your money"))
-//   }, 2000) // represent two days
-// })
+console.log('####################')
+// TODO: example 4
 
-// promise
-//   .then((money) => {
-//     console.log('Yay!, i got my money back')
-//     return money
-//   })
-//   .then(moneyReceived)
-//   .then((extramoney) => {
-//     if (extramoney) {
-//       console.log(
-//         'Wow my friend is generous ❤️, now i can buy something with the extra amount'
-//       )
-//     }
-//     console.log('Deposit the lended amount back to the bank')
-//   })
-//   .catch((error) => {
-//     console.error('😢', error.message)
-//   })
-//   .finally(() => {
-//     console.log('it will be called irrespective of the outcome')
-//   })
+// Successful and rejected scenario
+moneyLended = 10_000
+let promiseFour = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(moneyLended)
+    // reject(new Error("4. I lied, you're not getting your money"))
+  }, 2000)
+})
 
-// function moneyReceived(amountReceived) {
-//   return new Promise((resolve, reject) => {
-//     if (amountReceived > moneyLended) {
-//       resolve(amountReceived - moneyLended)
-//     } else if (amountReceived < moneyLended) {
-//       reject(new Error('Oh wait, i have not received the entire amount'))
-//     } else {
-//       resolve(0)
-//     }
-//   })
-// }
+promiseFour.then(
+  (money) => {
+    console.log('4. Yay!, i got my money back')
+    return money
+  },
+  (error) => {
+    console.error('😢', error.message)
+  }
+)
 
-// convertAmount(from, to = 5) => response
-// response => structured data {
-//   info: {
-//     rate: 81
-//   },
-//   result: 5 * 81
-// }
+function moneyReceived(amountReceived) {
+  return new Promise((resolve, reject) => {
+    if (amountReceived > moneyLended) {
+      resolve(amountReceived - moneyLended)
+    } else if (amountReceived < moneyLended) {
+      reject(new Error('Oh wait, i have not received the entire amount'))
+    } else {
+      resolve(0)
+    }
+  })
+}
 
-// Response in structured formats
-// JSON: JavaScript Object Notation (most used)
-// XML
-// Text
+console.log('----- END -----')
 
-let object1 = {
+console.log('\n')
+console.log('####################')
+// TODO: example 5
+// finally function, it will be called irrespective of the outcome
+// finally is widely used for API requests
+
+moneyLended = 10_000
+let promiseFive = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10_000)
+    // reject(new Error("I lied, you're not getting your money"))
+  }, 2000)
+})
+
+promiseFive
+  .then((money) => {
+    console.log('Yay!, i got my money back')
+    return money
+  })
+  .then(moneyReceived)
+  .then((extramoney) => {
+    if (extramoney) {
+      console.log(
+        'Wow my friend is generous ❤️, now i can buy something with the extra amount'
+      )
+    }
+    console.log('Deposit the lended amount back to the bank')
+  })
+  .catch((error) => {
+    console.error('😢', error.message)
+  })
+  .finally(() => {
+    console.log('it will be called irrespective of the outcome')
+  })
+
+function moneyReceived(amountReceived) {
+  return new Promise((resolve, reject) => {
+    if (amountReceived > moneyLended) {
+      resolve(amountReceived - moneyLended)
+    } else if (amountReceived < moneyLended) {
+      reject(new Error('Oh wait, i have not received the entire amount'))
+    } else {
+      resolve(0)
+    }
+  })
+}
+
+console.log('----- END -----')
+
+// ! convertAmount(from, to = 5) => response
+// ! response => structured data {
+// !   info: {
+// !     rate: 81
+// !   },
+// !  result: 5 * 81
+// ! }
+
+// TODO Response in structured formats
+// ? JSON: JavaScript Object Notation (most used)
+// ? XML
+// ? Text
+
+let object = {
   info: {
     rate: 81,
   },
   result: 5 * 81,
 }
-JSON.stringify(object1)
-// '{"info":{"rate":81},"result":405}'
-JSON.parse('{"info":{"rate":81},"result":405}')
-// {info: {…}, result: 405}
-// info
-// :
-// {rate: 81}
-// result
-// :
-// 405
-// [[Prototype]]
-// :
-// Object
+JSON.stringify(object)
 
-// Promise API example theory
-/**
-call api => request => promise
-response => json || error
-- calling an API
-- Requesting for some information
-- In the mean time the API time is taking to get that information we get a Promise
-- Once the result is available we will get a response
-- Once the response is available the promise will be settled
-- We will either get the JSON
-- Or well get some error
- */
+console.log('1. JSON.stringify(object):')
+console.log(JSON.stringify(object))
+
+console.log('2. JSON.parse(JSON.stringify(object)):')
+console.log(JSON.parse(JSON.stringify(object)))
+
+console.log('3. JSON.parse({"info":{"rate":81},"result":405}):')
+console.log(JSON.parse('{"info":{"rate":81},"result":405}'))
+
+// TODO Promise API example theory
+
+// ? call api => request => promise
+// ? response => json || error
+
+//* 1. calling an API
+//* 2. Requesting for some information
+//* 3. In the mean time the API time is taking to get that information we get a Promise
+//* 4. Once the result is available we will get a response
+//* 5. Once the response is available the promise will be settled
+//* 6. We will either get the JSON
+//* 7. Or well get some error
