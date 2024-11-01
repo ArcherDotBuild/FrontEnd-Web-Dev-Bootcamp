53c80af36c52b86634bf3c3fbe7102fab1550231
ElfGod <albertogx@icloud.com>
2024-10-31 23:24:00 -0500

    day 13 weather app part2

--- day-13/index.js
+++ day-13/index.js
@@ -16,14 +16,27 @@ const getCurrentWeatherData = async () => {
 // a variable called city that holds the value of name.
 const getHourlyForecast = async ({ name: city }) => {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=metric`
   )
   const data = await response.json()
-  console.log(data)
+  console.log('data: ', data)
+  return data.list.map((forecast) => {
+    const {
+      main: { temp, temp_max, temp_min },
+      dt,
+      dt_txt,
+      weather: [{ description, icon }],
+    } = forecast
+    return { temp, temp_max, temp_min, dt, dt_txt, description, icon }
+  })
 }
 
-// Formatting the temperature
+// * Formatting the temperature
 const formatTemperature = (temp) => `${temp?.toFixed(1)}°`
+// * Creating the icon
+const createIconUrl = (icon) =>
+  `http://openweathermap.org/img/wn/${icon}@2x.png`
+// ********************
 
 const loadCurrentForecast = ({
   name,
@@ -48,13 +61,49 @@ const loadCurrentForecast = ({
   )}`
 }
 
+const loadHourlyForecast = (hourlyForecast) => {
+  console.log('hourlyForecast: ', hourlyForecast)
+  let dataFor12Hours = hourlyForecast.slice(1, 13)
+  const hourlyContainer = document.querySelector('.hourly-container')
+  let innerHTMLString = ``
+
+  for (let { temp, icon, dt_txt } of dataFor12Hours) {
+    console.log('dt_txt: ', dt_txt)
+    innerHTMLString += `
+      <article>
+        <h3 class="time">${dt_txt.split(' ')[1]}</h3>
+        <img class="icon" src="${createIconUrl(icon)}" alt="icon" />
+        <p class="hourly-map">${formatTemperature(temp)}</p>
+      </article>
+    `
+  }
+  hourlyContainer.innerHTML = innerHTMLString
+}
+
+const loadFeelsLike = ({ main: { feels_like } }) => {
+  const container = document.querySelector('#feels-like')
+  container.querySelector('.feels-like-temp').textContent =
+    formatTemperature(feels_like)
+}
+
+const loadHumidity = ({ main: { humidity } }) => {
+  const container = document.querySelector('#humidity')
+  container.querySelector('.humidity-value').textContent = `${humidity} %`
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
-  preventDefault()
+  // preventDefault()
   // console.log(await getCurrentWeatherData())
 
   const currentWeather = await getCurrentWeatherData()
   loadCurrentForecast(currentWeather)
-  getHourlyForecast(currentWeather)
+
+  const hourlyForecast = await getHourlyForecast(currentWeather)
+  loadHourlyForecast(hourlyForecast)
+
+  loadFeelsLike(currentWeather)
+
+  loadHumidity(currentWeather)
 })
 
 /*
@@ -156,5 +205,173 @@ JSON format API response example
     "id": 3689147,
     "name": "Barranquilla",
     "cod": 200
+}
+*/
+
+/*                     
+{
+  "cod": "200",
+  "message": 0,
+  "cnt": 40,
+  "list": [
+    {
+      "dt": 1647345600,
+      "main": {
+        "temp": 287.39,
+        "feels_like": 286.38,
+        "temp_min": 286.69,
+        "temp_max": 287.39,
+        "pressure": 1021,
+        "sea_level": 1021,
+        "grnd_level": 1018,
+        "humidity": 58,
+        "temp_kf": 0.7
+      },
+      "weather": [
+        {
+          "id": 803,
+          "main": "Clouds",
+          "description": "broken clouds",
+          "icon": "04d"
+        }
+      ],
+      "clouds": {
+        "all": 71
+      },
+      "wind": {
+        "speed": 3.08,
+        "deg": 128,
+        "gust": 4.3
+      },
+      "visibility": 10000,
+      "pop": 0,
+      "sys": {
+        "pod": "d"
+      },
+      "dt_txt": "2022-03-15 12:00:00"
+    },
+    {
+      "dt": 1647356400,
+      "main": {
+        "temp": 287.09,
+        "feels_like": 286.13,
+        "temp_min": 286.5,
+        "temp_max": 287.09,
+        "pressure": 1021,
+        "sea_level": 1021,
+        "grnd_level": 1016,
+        "humidity": 61,
+        "temp_kf": 0.59
+      },
+      "weather": [
+        {
+          "id": 803,
+          "main": "Clouds",
+          "description": "broken clouds",
+          "icon": "04d"
+        }
+      ],
+      "clouds": {
+        "all": 81
+      },
+      "wind": {
+        "speed": 3.28,
+        "deg": 168,
+        "gust": 3.96
+      },
+      "visibility": 10000,
+      "pop": 0,
+      "sys": {
+        "pod": "d"
+      },
+      "dt_txt": "2022-03-15 15:00:00"
+    },
+    {
+      "dt": 1647367200,
+      "main": {
+        "temp": 285.44,
+        "feels_like": 284.6,
+        "temp_min": 284.47,
+        "temp_max": 285.44,
+        "pressure": 1020,
+        "sea_level": 1020,
+        "grnd_level": 1016,
+        "humidity": 72,
+        "temp_kf": 0.97
+      },
+      "weather": [
+        {
+          "id": 804,
+          "main": "Clouds",
+          "description": "overcast clouds",
+          "icon": "04d"
+        }
+      ],
+      "clouds": {
+        "all": 90
+      },
+      "wind": {
+        "speed": 2.7,
+        "deg": 183,
+        "gust": 5.59
+      },
+      "visibility": 10000,
+      "pop": 0,
+      "sys": {
+        "pod": "d"
+      },
+      "dt_txt": "2022-03-15 18:00:00"
+    },
+    .....
+        {
+      "dt": 1647766800,
+      "main": {
+        "temp": 282.42,
+        "feels_like": 280,
+        "temp_min": 282.42,
+        "temp_max": 282.42,
+        "pressure": 1036,
+        "sea_level": 1036,
+        "grnd_level": 1033,
+        "humidity": 60,
+        "temp_kf": 0
+      },
+      "weather": [
+        {
+          "id": 802,
+          "main": "Clouds",
+          "description": "scattered clouds",
+          "icon": "03d"
+        }
+      ],
+      "clouds": {
+        "all": 39
+      },
+      "wind": {
+        "speed": 4.58,
+        "deg": 83,
+        "gust": 8.45
+      },
+      "visibility": 10000,
+      "pop": 0,
+      "sys": {
+        "pod": "d"
+      },
+      "dt_txt": "2022-03-20 09:00:00"
+    }
+  ],
+  "city": {
+    "id": 2643743,
+    "name": "London",
+    "coord": {
+      "lat": 51.5085,
+      "lon": -0.1257
+    },
+    "country": "GB",
+    "population": 1000000,
+    "timezone": 0,
+    "sunrise": 1647324902,
+    "sunset": 1647367441
+  }
 }
 */
