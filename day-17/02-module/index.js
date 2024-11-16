f3440d06efd547f75ace8df025e83ece63909dd6
ElfGod <albertogx@icloud.com>
2024-11-15 23:55:12 -0500

    day 17:  part 3, ES6 Modules, Frontend Tooling - Webpack & Vite

--- day-17/02-module/index.js
+++ day-17/02-module/index.js
@@ -3,8 +3,31 @@ import { publicVariable } from './somefile.js'
 
 console.log('publicVariable: ', publicVariable)
 
+console.log('\n')
 // ! privateVariable is not defined
-// console.log('privateVariable: ', privateVariable) 
+// console.log('privateVariable: ', privateVariable)
 
+console.log('\n')
 import { usefulFunction } from './somefile.js'
 console.log(`${usefulFunction()} ${publicVariable}`)
+
+console.log('\n')
+import { exportAsYouDefine } from './somefile.js'
+console.log(exportAsYouDefine())
+
+console.log('\n')
+import defaultFunctionABC from './somefile.js'
+console.log('defaultFunctionABC: ', defaultFunctionABC())
+
+console.log('\n')
+// * import multiple things
+// ? import defaultFunctionABC, { usefulFunction } from './somefile.js'
+import { exportOne, exportTwo } from './somefile.js'
+console.log(exportOne, exportTwo)
+
+console.log('\n')
+// * rename the import as something else
+// using (as) keyword you can give an alias to a named import
+import { renameImport as ReNaMeImPoRt } from './somefile.js'
+console.log(ReNaMeImPoRt())
+
--- day-17/02-module/somefile.js
+++ day-17/02-module/somefile.js
@@ -1,7 +1,28 @@
 export let publicVariable = 10
 let privateVariable = 20
 
-// named exports
+export let exportOne = 1
+export let exportTwo = 2
+
+// Named Exports
 export function usefulFunction() {
   console.log('usefulFunction')
+}
+
+// Export as You Define
+function exportAsYouDefine() {
+  console.log('exportAsYouDefine')
+}
+export { exportAsYouDefine }
+
+// Default Export
+function defaultExport() {
+  console.log('defaultExport')
+}
+
+export default defaultExport
+
+// rename import
+export function renameImport() {
+  console.log('renameImport')
 }
