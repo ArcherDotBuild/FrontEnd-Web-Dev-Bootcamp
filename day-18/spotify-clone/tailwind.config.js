23d7e89f26102c833a05ec899d6456c35767899b
ElfGod <albertogx@icloud.com>
2024-11-20 03:21:34 -0500

    day 18, tailwind setup done / Spotify Clone Part 1

---
+++ day-18/spotify-clone/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       green: '#1DB954',
+  //       'black-base': '#121212',
+  //       'black-primary': '#191414',
+  //       'black-secondary': '#171818',
+  //       'light-black': '#282828',
+  //       primary: '#FFFFFF',
+  //       secondary: '#b3b3b3',
+  //       gray: '#535353',
+  //     },
+  //     gridTemplateColumns: {
+  //       'auto-fill-cards': 'repeat(auto-fill, minmax(200px, 1fr))',
+  //     },
+  //   },
+  // },
+  // plugins: [require('@tailwindcss/line-clamp')],
+}
---
+++ day-18/spotify-clone/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig, loadEnv } from 'vite'
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    root: 'src',
+    // build: {
+    //   outDir: '../dist',
+    //   rollupOptions: {
+    //     input: {
+    //       main: resolve(__dirname, 'src/index.html'),
+    //       dashboard: resolve(__dirname, 'src/dashboard/dashboard.html'),
+    //       login: resolve(__dirname, 'src/login/login.html'),
+    //     },
+    //   },
+    // },
+  }
+})
