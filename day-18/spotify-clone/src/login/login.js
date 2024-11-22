298f925646a85bb668769b86fa45a1f01a7ae946
ElfGod <albertogx@icloud.com>
2024-11-21 23:45:29 -0500

    day 18, trying to config the ENV

---
+++ day-18/spotify-clone/src/login/login.js
@@ -0,0 +1,10 @@
+// require('dotenv').config()
+
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+console.log(import.meta.env.VITE_APIKEY)
+
+// let apikey = process.env.apikey
+
+document.addEventListener('DOMContentLoeaded', () => {})
