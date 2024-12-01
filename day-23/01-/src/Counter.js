151606c3371f86a88b2617c745d14256419f7c15
ElfGod <albertogx@icloud.com>
2024-12-01 02:27:41 -0500

    day 23: counter

--- day-23/01-/src/Counter.js
+++ day-23/01-/src/Counter.js
@@ -3,11 +3,28 @@ import { useState } from 'react'
 
 const Counter = () => {
   const [count, setCount] = useState(0)
+  console.log('counter rendered', count)
 
+  // const handleClick = () => {
+  //   // setCount(count + 1)
+  //   // setCount(count + 4)
+  //   setCount(count + 1) // 0 + 1 = 1
+  //   setCount(count + 1) // 0 + 1 = 1
+  //   setCount(count + 1) // 0 + 1 = 1
+  //   setCount(count + 1) // 0 + 1 = 1
+  //   setCount(count + 1) // 0 + 1 = 1
+  // }
+
+  // updater Function
   const handleClick = () => {
-    // setCount(count + 1)
-    setCount(count + 4)
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+    setCount((prevCount) => prevCount + 1)
+    console.log('after click', count)
   }
+
   return (
     <div>
       <p>{count}</p>
