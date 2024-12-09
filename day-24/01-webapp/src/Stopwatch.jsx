f089a74adc2388b9827158a99f7cfa197c902e6f
ElfGod <albertogx@icloud.com>
2024-12-08 23:57:55 -0500

    day 24: tracklap example

--- day-24/01-webapp/src/Stopwatch.jsx
+++ day-24/01-webapp/src/Stopwatch.jsx
@@ -1,9 +1,11 @@
 import React, { useRef, useState } from 'react'
 
-// 19 minutes
 const Stopwatch = () => {
+  console.log('Stopwatch rerendered')
   const startTime = useRef(null)
+  const intervalRef = useRef(null)
   const [now, setNow] = useState(null)
+  const [laps, setLaps] = useState([])
 
   // How many seconds have elapsed since the timer has started
   let secondsElapsed = 0
@@ -11,15 +13,18 @@ const Stopwatch = () => {
   function startStopwatch() {
     // Date.now(): time in milliseconds
     // When was start button clicked, point in time
+    // Keep track of the value
     startTime.current = Date.now()
 
     // Every 10 milliseconds we will be updating the value of now
-    setInterval(() => {
+    intervalRef.current = setInterval(() => {
       setNow(Date.now())
     }, 10)
   }
 
-  function stopStopwatch() {}
+  function stopStopwatch() {
+    clearInterval(intervalRef.current)
+  }
 
   // Divide / 1000 to get the value in seconds
   // because (now) and (startTime.current) are both in milliseconds
@@ -31,7 +36,18 @@ const Stopwatch = () => {
         <h1>{secondsElapsed.toFixed(3)}</h1>
         <button onClick={startStopwatch}>Start</button>
         <button onClick={stopStopwatch}>Stop</button>
+        <button onClick={trackLap}>Lap</button>
       </section>
+      <h2>Laps</h2>
+      {/* Conditional rendering */}
+      {/* Check if (lap) array has any items */}
+      {laps?.length ? (
+        <article>
+          {laps.map((lap) => (
+            <p key={lap}>{lap}</p>
+          ))}
+        </article>
+      ) : null}
     </>
   )
 }
