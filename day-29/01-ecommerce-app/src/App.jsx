9001587bb9cd4835c9f6be6ed3b2db1c85f61347
Alberto Guzman <albertogx@icloud.com>
2025-01-24 23:56:08 -0500

    day 29: ecom app pt2

--- day-29/01-ecommerce-app/src/App.jsx
+++ day-29/01-ecommerce-app/src/App.jsx
@@ -1,6 +1,8 @@
 // import 'App.css'
 import Button from '@mui/material/Button'
 
+// return <Button variant='outlined'>Hello @ArcherDotBuild</Button>
+
 function App() {
   return <Button variant='contained'>Hello @ArcherDotBuild</Button>
 }
