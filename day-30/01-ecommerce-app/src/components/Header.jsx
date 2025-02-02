a92adad1354c012ece8cfd348912abe208d396af
Alberto Guzman <albertogx@icloud.com>
2025-02-02 04:47:45 -0500

    day 29: ecom app pt6

--- day-30/01-ecommerce-app/src/components/Header.jsx
+++ day-30/01-ecommerce-app/src/components/Header.jsx
@@ -1,28 +1,48 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar'
-import { Badge, Box, Button, IconButton, Toolbar } from '@mui/material'
+import {
+  alpha,
+  Badge,
+  Box,
+  Button,
+  IconButton,
+  styled,
+  Toolbar,
+} from '@mui/material'
 import { Typography } from '@mui/material'
 import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp'
 import { useSelector } from 'react-redux'
 import { getItemCount } from '../utils'
 
+const Search = styled('section')(({ theme }) => ({
+  position: 'relative',
+  bordeRadius: theme.shape.borderRadius,
+  display: 'flex',
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+}))
+
+function SearchBar() {
+  return <Search></Search>
+}
+
 const Header = () => {
   const cartItems = useSelector((state) => state.cart?.value)
   const count = getItemCount(cartItems)
-  
+
   return (
     <AppBar position='sticky'>
       <Toolbar>
-        <Typography
-          variant='h6'
-          color='inherit'
-          sx={{
-            flexGrow: 1,
-          }}
-        >
-          Ecommerce App
+        <Typography variant='h6' color='inherit'>
+          Ecommerce
         </Typography>
-        <Box sx={{  display: {md: 'flex'} }}>
+        <SearchBar />
+        <Box sx={{ display: { md: 'flex' } }}>
           <IconButton
             size='large'
             aria-label='shows cart items count'
@@ -34,7 +54,6 @@ const Header = () => {
           </IconButton>
         </Box>
         <Button color='inherit'>Login</Button>
-        
       </Toolbar>
     </AppBar>
   )
