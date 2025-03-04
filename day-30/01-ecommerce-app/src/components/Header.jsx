7a20358d89749a698a7294a7a40ebd005ee4e4cc
Alberto Guzman <albertogx@icloud.com>
2025-03-03 23:58:01 -0500

    day 29: ecom app pt20

--- day-30/01-ecommerce-app/src/components/Header.jsx
+++ day-30/01-ecommerce-app/src/components/Header.jsx
@@ -32,7 +32,7 @@ const Search = styled('section')(({ theme }) => ({
   marginLeft: 0,
   width: '100%',
 }))
-// 1:22
+// 1:23
 function SearchBar() {
   const products = useSelector((state) => state.products?.value)
   const categories = useSelector((state) => state.categories?.value)
