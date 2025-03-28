2d26c8986e98f673932d6279f955738ec0ece139
Alberto Guzman <albertogx@icloud.com>
2025-03-28 04:48:46 -0500

    day 31: ecomm app pt03

--- day-31/01-ecommerce-app/src/pages/Cart.jsx
+++ day-31/01-ecommerce-app/src/pages/Cart.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import Card from '@mui/material/Card'
+import { useSelector } from 'react-redux'
+import { CardMedia } from '@mui/material'
 
 const Cart = () => {
+  const cart = useSelector((state) => state.cart?.value)
   return (
-    <div>Cart</div>
-    // 8 minute
+    <Container sx={{ py: 8 }}>
+      <Grid container>
+        <Grid item container>
+          {/* <pre>{JSON.stringify(cartItems, null, 2)}</pre> */}
+          {cart.map(({ product, quantity }) => {
+            const { title, id, price, description, rating, image } = product
+            return (
+              <Grid item key={id}>
+                <Card>
+                  <CardMedia component='img'></CardMedia>
+                </Card>
+              </Grid>
+            )
+          })}
+        </Grid>
+        <Grid item>
+          <Typography variant='h4'>Subtotal</Typography>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
