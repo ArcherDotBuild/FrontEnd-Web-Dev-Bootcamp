40b524f3b947e695e10eec779fdaa1134d9eff1f
Alberto Guzman <albertogx@icloud.com>
2025-03-30 05:24:22 -0500

    day 31: ecomm app pt04

--- day-31/01-ecommerce-app/src/pages/Cart.jsx
+++ day-31/01-ecommerce-app/src/pages/Cart.jsx
@@ -1,30 +1,63 @@
 import React from 'react'
+import { useTheme } from '@emotion/react'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Card from '@mui/material/Card'
 import { useSelector } from 'react-redux'
-import { CardMedia } from '@mui/material'
+import { Box, CardContent, CardMedia, Rating, TextField } from '@mui/material'
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart?.value)
+  const theme = useTheme()
   return (
     <Container sx={{ py: 8 }}>
-      <Grid container>
-        <Grid item container>
+      <Grid container spacing={2}>
+        <Grid item container spacing={2} md={8}>
           {/* <pre>{JSON.stringify(cartItems, null, 2)}</pre> */}
           {cart.map(({ product, quantity }) => {
             const { title, id, price, description, rating, image } = product
             return (
-              <Grid item key={id}>
-                <Card>
-                  <CardMedia component='img'></CardMedia>
+              <Grid item key={id} xs={12}>
+                <Card
+                  sx={{
+                    display: 'flex',
+                    py: 2,
+                    px: 2,
+                  }}
+                >
+                  <CardMedia
+                    component='img'
+                    image={image}
+                    sx={{
+                      height: theme.spacing(30),
+                      width: theme.spacing(30),
+                      objectFit: 'contain',
+                      pt: theme.spacing(),
+                    }}
+                    alt={title}
+                  />
+                  {/* 20 */}
+                  <CardContent>
+                    <Box
+                      sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+                    >
+                      <Typography variant='h6'>{title}</Typography>
+                      <Rating readOnly precision={0.5} value={rating.rate} />
+                      <form>
+                        <TextField
+                          label='Quantity'
+                          value={quantity}
+                        ></TextField>
+                      </form>
+                    </Box>
+                  </CardContent>
                 </Card>
               </Grid>
             )
           })}
         </Grid>
-        <Grid item>
+        <Grid ite md={4}>
           <Typography variant='h4'>Subtotal</Typography>
         </Grid>
       </Grid>
