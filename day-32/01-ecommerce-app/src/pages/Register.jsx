50243fa277a3c680491a90f7e65c3bfc3ddc8c2b
Alberto Guzman <albertogx@icloud.com>
2025-05-09 03:41:13 -0500

    ecom part4, register page pt4

--- day-32/01-ecommerce-app/src/pages/Register.jsx
+++ day-32/01-ecommerce-app/src/pages/Register.jsx
@@ -15,18 +15,51 @@ import {
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 
 const Register = () => {
-  return <Container component={'main'} maxWidth='xs'>
-    <CssBaseline />
-    <Box sx={{ mt: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Avatar sx={{
-        m: 1,
-        bgcolor: 'secondary.main',
-      }}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component={'h1'} variant='h5'></Typography>
-    </Box>
-  </Container>
+  return (
+    <Container component={'main'} maxWidth='xs'>
+      <CssBaseline />
+      <Box
+        sx={{
+          mt: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar
+          sx={{
+            m: 1,
+            bgcolor: 'secondary.main',
+          }}
+        >
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component={'h1'} variant='h5'></Typography>
+        <Box
+          component={'form'}
+          sx={{
+            mt: 3,
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid item sx={12}>
+              <TextField
+              autoComplete='given-name'
+              fullWidth
+              required
+              name='name'
+              id='name'
+              autoFocus
+              label='name'
+              >
+
+              </TextField>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 // 7 min
 export default Register
