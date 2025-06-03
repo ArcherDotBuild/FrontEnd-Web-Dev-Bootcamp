0c0252f80130c50c12332168eef8b4d581b8a2a9
Alberto Guzman <albertogx@icloud.com>
2025-06-03 02:28:44 -0500

    ecom part4, register page pt15

--- day-32/01-ecommerce-app/src/pages/Checkout.jsx
+++ day-32/01-ecommerce-app/src/pages/Checkout.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
-import Toolbar from '@mui/material/Toolbar'
-import Paper from '@mui/material/Paper'
-import Stepper from '@mui/material/Stepper'
-import Step from '@mui/material/Step'
-import StepLabel from '@mui/material/StepLabel'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import { useState } from 'react'
-import AddressForm from '../components/AddressForm'
-import PaymentsForm from '../components/PaymentsForm'
-import ReviewForm from '../components/ReviewForm'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+  Box,
+  Container,
+  Toolbar,
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Button,
+  Typography,
+} from '@mui/material'
+// import AddressForm from '../components/AddressForm'
+// import PaymentsForm from '../components/PaymentsForm'
+// import ReviewForm from '../components/ReviewForm'
 import { useDispatch } from 'react-redux'
-import { clearCart } from '../feature/cart-slice'
-import { clearCheckoutInformation } from '../feature/checkout-slice'
+// import { clearCart } from '../feature/cart-slice'
+// import { clearCheckoutInformation } from '../feature/checkout-slice'
 import { Link } from 'react-router-dom'
 
 const steps = ['Shipping Address', 'Payment Details', 'Review Your Order']
@@ -29,18 +29,20 @@ const Checkout = () => {
         variant='outlined'
         sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
       >
-        <Typography component='h1' variant='h4' align='center'>Checkout</Typography>
+        <Typography component='h1' variant='h4' align='center'>
+          Checkout
+        </Typography>
         <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-            {steps.map((label) => (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            ))}
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
         </Stepper>
       </Paper>
     </Container>
   )
 }
-// 28
+// 29
 
 export default Checkout
