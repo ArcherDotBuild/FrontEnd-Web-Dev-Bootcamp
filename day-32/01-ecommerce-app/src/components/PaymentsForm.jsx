c09d1f81421379e6ee60d34b1c8fbeb46fedaa13
Alberto Guzman <albertogx@icloud.com>
2025-06-19 02:20:01 -0500

    ecom part4, register page pt26

--- day-32/01-ecommerce-app/src/components/PaymentsForm.jsx
+++ day-32/01-ecommerce-app/src/components/PaymentsForm.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
+import { Typography, Box, Grid } from '@mui/material'
 
 const PaymentsForm = () => {
   return (
-    <div>PaymentsForm</div>
+    <>
+      <Typography variant='h6' gutterBottom>
+        Payment method
+      </Typography>
+      <Box component='form' onChange={handleChange}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              name='name'
+              id='name'
+              variant='standard'
+              required
+              label='Name on Card'
+              fullWith
+              autoComplete='cc-name'
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              name='cardNumber'
+              id='cardNumber'
+              variant='standard'
+              required
+              label='Card Number'
+              fullWith
+              autoComplete='cc-number'
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              name='expDate'
+              id='expDate'
+              variant='standard'
+              required
+              label='Expiry Date'
+              fullWith
+              autoComplete='cc-exp'
+            ></TextField>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   )
 }
 
-// 105
+// 111
 export default PaymentsForm
