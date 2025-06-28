306b8d8723104d5e143b004b64d2d676cac56ecd
Alberto Guzman <albertogx@icloud.com>
2025-06-28 01:45:03 -0500

    ecom part4, register page pt34

--- day-32/01-ecommerce-app/src/components/ReviewForm.jsx
+++ day-32/01-ecommerce-app/src/components/ReviewForm.jsx
@@ -3,12 +3,21 @@ import { Typography, List, ListItem, ListItemText, Grid } from '@mui/material'
 import { useSelector } from 'react-redux'
 
 const ReviewForm = () => {
-  const cart = useSelector(state => state.cart)
+  const cart = useSelector((state) => state.cart.value)
   const address = useSelector((state) => state.checkout.address)
-  const payment = useSelector(state => state.checkout.payment)
+  const payment = useSelector((state) => state.checkout.payment)
 
   return (
-    <div>ReviewForm</div>
+    <>
+      <Typography variant='h6' gutterBottom>
+        Order summary
+      </Typography>
+      <LIST>
+        {cart?.map(({ product, quantity }) => (
+          <ListItem key={product.title} sx={{py: 1, px: 0}}></ListItem>
+        ))}
+      </LIST>
+    </>
   )
 }
 
