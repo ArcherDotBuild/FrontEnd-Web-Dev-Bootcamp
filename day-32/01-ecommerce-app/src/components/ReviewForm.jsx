39df3544124651f9347d6df2efaabb1743d3d4bf
Alberto Guzman <albertogx@icloud.com>
2025-06-29 02:47:32 -0500

    ecom part4, register page pt35

--- day-32/01-ecommerce-app/src/components/ReviewForm.jsx
+++ day-32/01-ecommerce-app/src/components/ReviewForm.jsx
@@ -1,24 +1,55 @@
 import React from 'react'
-import { Typography, List, ListItem, ListItemText, Grid } from '@mui/material'
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Grid,
+  useTheme,
+} from '@mui/material'
 import { useSelector } from 'react-redux'
-
+import { getSubtotal } from '../utils'
 const ReviewForm = () => {
   const cart = useSelector((state) => state.cart.value)
   const address = useSelector((state) => state.checkout.address)
   const payment = useSelector((state) => state.checkout.payment)
+  const theme = useTheme()
 
   return (
     <>
       <Typography variant='h6' gutterBottom>
         Order summary
       </Typography>
-      <LIST>
+      <List disablePadding>
         {cart?.map(({ product, quantity }) => (
-          <ListItem key={product.title} sx={{py: 1, px: 0}}></ListItem>
+          <ListItem key={product.title} sx={{ py: 1, px: 0 }}>
+            <ListItemText
+              sx={{
+                '& .MuiListItemText-primary': {
+                  fontWeight: 500,
+                },
+                '& .MuiListItemText-secondary': {
+                  fontSize: theme.spacing(2),
+                },
+              }}
+              primary={product.title}
+              secondary={`Qty: ${quantity}`}
+            />
+            <Typography variant='body2'>
+              {getSubtotal([{ product, quantity }])?.toFixed(2)}
+            </Typography>
+          </ListItem>
         ))}
-      </LIST>
+        <ListItem sx={{ py: 1, px: 0 }}>
+          <ListItemText primary='Total' />
+          <Typography variant='subtitle1' sx={{ mt: 2 }}>
+            {getSubtotal(cart)?.toFixed(2)}
+          </Typography>
+        </ListItem>
+      </List>
     </>
   )
 }
-
+// 1.27
+// 28
 export default ReviewForm
