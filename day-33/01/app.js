7b98f17ef9549b18b2cf17effb5ae4167d01a839
Alberto Guzman <albertogx@icloud.com>
2025-07-16 01:39:21 -0500

    day-33: string to number error

---
+++ day-33/01/app.js
@@ -0,0 +1,18 @@
+let somevariable = 'alberto'
+console.log(somevariable)
+somevariable = 'elfgodd'
+console.log(somevariable)
+
+// node app.js
+/*
+output:
+alberto
+elfgodd
+*/
+
+console.log(somevariable.toUpperCase())
+somevariable = 17
+// console.log(somevariable.toUpperCase())
+// TypeError: somevariable.toUpperCase is not a function
+
+
