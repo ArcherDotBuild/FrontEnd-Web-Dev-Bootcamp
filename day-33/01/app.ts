aa06d42030c54151397ee88797ef4044b083f9ac
Alberto Guzman <albertogx@icloud.com>
2025-07-25 23:45:15 -0500

    day-33: next chapter

--- day-33/01/app.ts
+++ day-33/01/app.ts
@@ -20,3 +20,5 @@ function addNumbersTS(firstNumber: number, secondNumber: number): number {
 console.log(addNumbersTS(2, 4))
 // Argument of type 'string' is not assignable to parameter of type 'number'.
 // console.log(addNumbersTS(3, '7'))
+
+// 6m
