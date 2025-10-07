3649fc762a97fb8d47d65006ed7fed84e6c63dc1
Alberto Guzman <albertogx@icloud.com>
2025-10-06 23:54:31 -0500

    day33: todosReducer pt14

--- day-33/03/todo-app-ts/src/todosReducer.ts
+++ day-33/03/todo-app-ts/src/todosReducer.ts
@@ -1,29 +1,32 @@
 import { Todo } from './TodosProvider'
 
-export type Action = {
-  type: 'add'
-  id: number
-  text: string
-} | { type: 'change', todo: Todo}
+export type Action =
+  | {
+      type: 'add'
+      id: number
+      text: string
+    }
+  | { type: 'change'; todo: Todo }
+  | { type: 'remove'; id: number }
 
 export default function todosReducer(state: Todo[], action: Action): Todo[] {
   const { type } = action
-  switch(type) {
-    case 'add': { 
+  switch (type) {
+    case 'add': {
       const { id, text } = action
-      return [...state, {id, text, done: false}]
+      return [...state, { id, text, done: false }]
     }
 
- case 'change':
-  const updatedTodo = action.todo
-  return state.map((existingTodo) => {
-    if (existingTodo.id === updatedTodo.id) {
-      return updatedTodo
-    } else {
-      return existingTodo
-    }
-  })
+    case 'change':
+      const updatedTodo = action.todo
+      return state.map((existingTodo) => {
+        if (existingTodo.id === updatedTodo.id) {
+          return updatedTodo
+        } else {
+          return existingTodo
+        }
+      })
   }
 }
 
-// 1.16mins
+// 1.17mins
