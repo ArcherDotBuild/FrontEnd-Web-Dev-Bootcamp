7cdd05237d5a7c243fdff88c3be365526fb7a626
Alberto Guzman <albertogx@icloud.com>
2025-11-30 23:48:35 -0500

    day34 pt3 starting to config env

--- day-34/netflix-clone/src/App.tsx
+++ day-34/netflix-clone/src/App.tsx
@@ -1,13 +1,25 @@
-import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
-    </>
+function AppRouter() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route
+        path="/"
+        element={<h1 className="text-3xl font-bold underline">Hello world!</h1>}
+      />,
+    ),
   );
+
+  return <RouterProvider router={router} />;
 }
 
-export default App;
+export default function App() {
+  return <AppRouter />;
+}
+
+// 12m
